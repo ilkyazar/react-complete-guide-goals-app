db168805c22fa181b0488bf99fd662be8db3ec55
ilkyazar <ilkyaz.arabaci@gmail.com>
2022-06-09 18:54:18 +0300

    add css modules for course input

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
-import styled from 'styled-components';
-
+import styles from './CourseInput.module.css';
 import Button from '../../UI/Button/Button';
-import './CourseInput.css';
+
+/* import styled from 'styled-components';
 
 const FormControl = styled.div`
   margin: 0.5rem 0;
@@ -27,7 +27,7 @@ const FormControl = styled.div`
     background: #fad0ec;
     border-color: #8b005d;
   }
-`;
+`; */
 
 const CourseInput = (props) => {
   const [enteredValue, setEnteredValue] = useState('');
@@ -51,10 +51,14 @@ const CourseInput = (props) => {
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <FormControl invalid={!isValid}>
+      <div
+        className={`${styles['form-control']} ${
+          !isValid && styles.invalid
+        }`}
+      >
         <label>Course Goal</label>
         <input type="text" onChange={goalInputChangeHandler} />
-      </FormControl>
+      </div>
       <Button type="submit">Add Goal</Button>
     </form>
   );
